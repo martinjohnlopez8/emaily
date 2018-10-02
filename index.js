c034988cd72ccb3389e315105c14da78e609c874
unknown <martin@philsmile.com>
2018-10-02 22:34:19 +0800

    'initial'

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
-const app = express();
+const mongoose = require('mongoose');
+const keys = require('./config/keys')
+require('./services/passport');
 
-app.get('/', (req, res) => {
-	res.send({'hi': 'sharon'});
-});
+mongoose.connect(keys.mongoURI);
+
+const app = express();
+require('./routes/authRoutes')(app);
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
